cd799b8d39ba3c2663384ec34122348547dbad15
yurket <yurket2006@yandex.ru>
2015-06-01 22:58:18 +0300

    80 hackvm solved

---
+++ 80_i_will_be_execute/solve.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+
+
+def main():
+    answer = 7
+    for i in range(730):
+        answer += 9
+    print(answer)
+        
+
+
+if __name__ == '__main__':
+    main()
+
